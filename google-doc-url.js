7d643b7a75e9367f573260a91ae1c91d85293b24
ageor7 <github@dipot.com>
2022-03-14 22:45:04 +0200

    Update google-doc-url.js
    Google Sheet and API

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,9 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o7N0eJ7xoPNXQuo9yT-ZRt0fU9GEIMtIDpH8MmzhOzg/edit#gid=0';
+
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+// ageor 20220314
+//var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyDYhbA6ln51gbkwd1sltu5-WgDjRagAilk';
